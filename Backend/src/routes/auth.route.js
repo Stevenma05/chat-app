4a5c77f365ebd6e6fd45e956be60f94527990161
Stevenma05 <stevenmarcos1010@gmail.com>
2025-06-12 00:53:17 +0400

    Logout completed

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,10 +1,14 @@
 import express from "express"
-import { login, logout, signup } from "../controllers/auth.controller.js"
+import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js"
 
 const router = express.Router()
 
+//post requests
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+//put requests
+router.put("/update-profile", updateProfile);
 
 export default router;
