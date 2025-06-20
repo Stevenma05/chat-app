c7207f70d7aaffd048478df44b15b89637577d6a
Stevenma05 <stevenmarcos1010@gmail.com>
2025-06-21 00:41:53 +0400

    Made the chat UI and made it work without real time connection.

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useChatStore } from '../Store/useChatStore.js'
+import Sidebar from '../compoenents/Sidebar.jsx';
+import NoChatSelected from '../compoenents/NoChatSelected.jsx';
+import ChatContainer from '../compoenents/ChatContainer.jsx';
 
 const HomePage = () => {
+  
+  const {selectedUser} = useChatStore();
+  
   return (
-    <div>
-      HomePage
+    <div className='h-screen bg-base-200'>
+      <div className='flex items-center justify-center pt-20 px-4'>
+        <div className='bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]'>
+          <div className='flex h-full rounded-lg overflow-hidden'>
+            <Sidebar />
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
